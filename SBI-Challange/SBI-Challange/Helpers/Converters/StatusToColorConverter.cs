0fa5e4d67c95df4e71aef45db847215264a5b648
scorpion712 <d.lautaro712@gmail.com>
2022-12-24 18:08:06 -0300

    Add converter to show status color

---
+++ SBI-Challange/SBI-Challange/Helpers/Converters/StatusToColorConverter.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Text;
+using Xamarin.Forms;
+
+namespace SBIChallange.Helpers.Converters
+{
+    public class StatusToColorConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is int)
+            {
+                var status = (int) value;
+                return status == 0 ? Application.Current.Resources["NotActiveColor"] : Application.Current.Resources["ActiveColor"];
+            }
+            return Application.Current.Resources["NotActiveColor"];
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
