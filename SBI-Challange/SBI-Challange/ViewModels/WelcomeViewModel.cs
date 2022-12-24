cb6a672de0ba3a720154c3a8b08637d7e5862e9f
scorpion712 <d.lautaro712@gmail.com>
2022-12-24 16:50:03 -0300

    Rename page and view model

---
+++ SBI-Challange/SBI-Challange/ViewModels/WelcomeViewModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows.Input;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace SBI_Challange.ViewModels
+{
+    public class WelcomeViewModel : BaseViewModel
+    {
+        public string LogoURL { get => "https://www.sbi-technology.com/assets/images/sitio/logo.svg";  }
+
+        public WelcomeViewModel()
+        {
+            Title = "About";
+        }
+
+    }
+}
--- SBI-Challange/SBI-Challange/Views/WelcomePage.xaml.cs
+++ SBI-Challange/SBI-Challange/Views/WelcomePage.xaml.cs
@@ -5,9 +5,9 @@ using Xamarin.Forms.Xaml;
 
 namespace SBI_Challange.Views
 {
-    public partial class AboutPage : ContentPage
+    public partial class WelcomePage : ContentPage
     {
-        public AboutPage()
+        public WelcomePage()
         {
             InitializeComponent();
         }
