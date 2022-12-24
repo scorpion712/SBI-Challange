a6d5bc59bff7c32cdcd02a6e3ce4e9c3d587de44
scorpion712 <d.lautaro712@gmail.com>
2022-12-24 16:47:32 -0300

    Add mapping properties to model using json

--- SBI-Challange/SBI-Challange/Models/User.cs
+++ SBI-Challange/SBI-Challange/Models/User.cs
@@ -1,13 +1,19 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 
 namespace SBI_Challange.Models
 {
     public class User
     {
+        [JsonProperty("Id")]
         public string Id { get; set; }
+        [JsonProperty("Name")]
         public string Name { get; set; }
+        [JsonProperty("Email")]
         public string Email { get; set; }
+        [JsonProperty("Phone")]
         public string Phone { get; set; }
+        [JsonProperty("Status")]
         public int Status { get; set; }
     }
 }
