1cc19a6bebf7095812ee2afcb2688fafaa873179
scorpion712 <d.lautaro712@gmail.com>
2022-12-24 17:27:48 -0300

    Add User model

---
+++ SBI-Challange/SBI-Challange/Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace SBI_Challange.Models
+{
+    public class User
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public string Phone { get; set; }
+        public int Status { get; set; }
+    }
+}
