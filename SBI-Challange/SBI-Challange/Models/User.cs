331369c5e52c0f52c9ae569d69e623e6d0f94963
scorpion712 <d.lautaro712@gmail.com>
2022-12-24 17:17:09 -0300

    Add autogenerated id

--- SBI-Challange/SBI-Challange/Models/User.cs
+++ SBI-Challange/SBI-Challange/Models/User.cs
@@ -5,8 +5,17 @@ namespace SBI_Challange.Models
 {
     public class User
     {
+        // Note: I use this id because the API response doesn't contains an id, so use Guid to generate random id
+        private string _id="";
         [JsonProperty("Id")]
-        public string Id { get; set; }
+        public string Id
+        {
+            get => String.IsNullOrEmpty(_id) ? Guid.NewGuid().ToString() : _id;
+            set
+            {
+                if (_id.Equals(value)) return; _id = value;
+            }
+        }
         [JsonProperty("Name")]
         public string Name { get; set; }
         [JsonProperty("Email")]
