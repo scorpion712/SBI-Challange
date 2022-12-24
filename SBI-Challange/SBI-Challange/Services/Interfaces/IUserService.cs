35a9a62b076cb0e474c49abfccb6b06d260fefca
scorpion712 <d.lautaro712@gmail.com>
2022-12-24 16:47:53 -0300

    Add service to fetch users from API

---
+++ SBI-Challange/SBI-Challange/Services/Interfaces/IUserService.cs
@@ -0,0 +1,13 @@
+﻿using SBI_Challange.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SBIChallange.Services.Interfaces
+{
+    public interface IUserService
+    {
+        Task<IEnumerable<User>> GetUsers();
+    }
+}
