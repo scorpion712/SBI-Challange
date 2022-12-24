ae44ebf429aefb6eaffebd841a41de5f5801742a
Lautaro Diez <ldiez@q4tech.com>
2022-12-23 22:51:23 -0300

    Add behaviour

--- SBI-Challange/SBI-Challange/ViewModels/LoginViewModel.cs
+++ SBI-Challange/SBI-Challange/ViewModels/LoginViewModel.cs
@@ -1,13 +1,32 @@
 ﻿using SBI_Challange.Views;
-using System;
-using System.Collections.Generic;
-using System.Text;
+using System; 
 using Xamarin.Forms;
 
 namespace SBI_Challange.ViewModels
 {
     public class LoginViewModel : BaseViewModel
     {
+        #region Attributes
+        private string _username;
+        private string _password; 
+        #endregion
+
+        #region Properties
+        public string Username
+        {
+            get { return _username; }
+            set { if (_username == value) return; _username = value; OnPropertyChanged(); OnPropertyChanged(nameof(IsButtonEnabled)); }
+        }
+
+        public string Password
+        {
+            get { return _password; }
+            set { if (_password == value) return; _password = value; OnPropertyChanged(); OnPropertyChanged(nameof(IsButtonEnabled)); }
+        }
+
+        public bool IsButtonEnabled { get => !String.IsNullOrEmpty(Username) || !String.IsNullOrEmpty(Password); } 
+        #endregion
+
         public Command LoginCommand { get; }
 
         public LoginViewModel()
